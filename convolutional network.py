4c771d6aee1cdca601d9bab56faa4f97f454dff5
Lucas Palma <134965166+palmaz9@users.noreply.github.com>
2024-04-03 20:39:06 +0000

    using cifar10 test 1

--- convolutional network.py
+++ convolutional network.py
@@ -1,15 +1,15 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-# Load MNIST dataset
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+# Load CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 
-# Normalize and reshape the images
-train_images = train_images.reshape(-1, 28, 28, 1) / 255.0
-test_images = test_images.reshape(-1, 28, 28, 1) / 255.0
+# Normalize the images
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 # Convert labels to categorical one-hot encoding
 train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=10)
@@ -17,19 +17,19 @@ test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=10)
 
 # Build the CNN model
 model = Sequential()
-model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
-history = model.fit(train_images, train_labels, epochs=10, batch_size=32, validation_data=(test_images, test_labels))
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
 # Evaluate the model
 test_loss, test_acc = model.evaluate(test_images, test_labels)
